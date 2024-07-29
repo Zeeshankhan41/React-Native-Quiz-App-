4eda2da1839d15435327a63a22038bfcec09bda5
Muhammad Zeeshan <166248599+Zeeshankhan41@users.noreply.github.com>
2024-07-29 10:10:43 -0700

    Add files via upload

---
+++ .bundle/App.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+import Quiz from './Src/Quiz/Datalist/Quiz'
+
+const App = () => {
+  return (
+  
+  <Quiz/>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
+
+
+
